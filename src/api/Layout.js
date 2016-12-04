3bb7d915d0eab4fafd4bcae1aa8217f428a908ce
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-12-04 22:44:21 +0100

    subclass options

--- src/api/Layout.js
+++ src/api/Layout.js
@@ -1,15 +1,29 @@
+import isBoolean from 'd2-utilizr/lib/isBoolean';
+import isEmpty from 'd2-utilizr/lib/isEmpty';
 import isObject from 'd2-utilizr/lib/isObject';
+import isString from 'd2-utilizr/lib/isString';
 
 import { Layout as d2aLayout } from 'd2-analysis';
 
-export var Layout = function(refs, config, applyConfig, forceApplyConfig) {
+export var Layout = function(refs, c, applyConfig, forceApplyConfig) {
     var t = this;
     t.klass = Layout;
 
-    config = isObject(config) ? config : {};
+    c = isObject(c) ? c : {};
 
     // inherit
-    Object.assign(t, new d2aLayout(refs, config, applyConfig));
+    Object.assign(t, new d2aLayout(refs, c, applyConfig));
+
+    // options
+    t.showDataDescription = isBoolean(c.showDataDescription) ? c.showDataDescription : (isBoolean(c.showDataDescription) ? c.showDataDescription : true);
+    t.hideEmptyRows = isBoolean(c.hideEmptyRows) ? c.hideEmptyRows : false;
+
+    t.showHierarchy = isBoolean(c.showHierarchy) ? c.showHierarchy : false;
+
+    t.reduceLayout = isBoolean(c.reduceLayout) ? c.reduceLayout : false;
+
+    t.displayDensity = isString(c.displayDensity) && !isEmpty(c.displayDensity) ? c.displayDensity : refs.optionConfig.getDisplayDensity('normal').id;
+    t.fontSize = isString(c.fontSize) && !isEmpty(c.fontSize) ? c.fontSize : refs.optionConfig.getFontSize('normal').id;
 };
 
 Layout.prototype = d2aLayout.prototype;
